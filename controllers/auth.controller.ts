d7215a8fe16fa4e01e620688704de9abcae5187b
shiva <shivatadigadapa@gmail.com>
2023-09-28 05:50:04 +0530

    new back

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,277 +1,277 @@
-import crypto from "crypto";
-import { Prisma } from "@prisma/client";
-import { Request, Response, NextFunction } from "express";
-import { prisma } from "../server";
-const OTPAuth = require("otpauth");
+// import crypto from "crypto";
+// import { Prisma } from "@prisma/client";
+// import { Request, Response, NextFunction } from "express";
+// import { prisma } from "../server";
+// const OTPAuth = require("otpauth");
 
-import { encode } from "hi-base32";
-const bcrypt = require('bcrypt');
+// import { encode } from "hi-base32";
+// const bcrypt = require('bcrypt');
 
-const RegisterUser = async (req:Request, res:Response, next:NextFunction) => {
-  try {
-    const { name, email, password, role } = req.body;
+// const RegisterUser = async (req:Request, res:Response, next:NextFunction) => {
+//   try {
+//     const { name, email, password, role } = req.body;
 
-    // Hash the password before storing it in the database
-    const hashedPassword = await bcrypt.hash(password, 10); // 10 is the number of salt rounds
+//     // Hash the password before storing it in the database
+//     const hashedPassword = await bcrypt.hash(password, 10); // 10 is the number of salt rounds
 
-    await prisma.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword, // Store the hashed password
-        role,
-      },
-    });
+//     await prisma.user.create({
+//       data: {
+//         name,
+//         email,
+//         password: hashedPassword, // Store the hashed password
+//         role,
+//       },
+//     });
 
-    res.status(201).json({
-      status: 'success',
-      message: 'Registered successfully, please login',
-    });
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      if (error.code === 'P2002') {
-        return res.status(409).json({
-          status: 'fail',
-          message: 'Email already exists, please use another email address',
-        });
-      }
-    }
-    res.status(500).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
-};
+//     res.status(201).json({
+//       status: 'success',
+//       message: 'Registered successfully, please login',
+//     });
+//   } catch (error) {
+//     if (error instanceof Prisma.PrismaClientKnownRequestError) {
+//       if (error.code === 'P2002') {
+//         return res.status(409).json({
+//           status: 'fail',
+//           message: 'Email already exists, please use another email address',
+//         });
+//       }
+//     }
+//     res.status(500).json({
+//       status: 'error',
+//       message: error.message,
+//     });
+//   }
+// };
 
-const LoginUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { email, password } = req.body;
+// const LoginUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const { email, password } = req.body;
 
-    const user = await prisma.user.findUnique({ where: { email } });
+//     const user = await prisma.user.findUnique({ where: { email } });
 
-    if (!user) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'No user with that email exists',
-      });
-    }
+//     if (!user) {
+//       return res.status(404).json({
+//         status: 'fail',
+//         message: 'No user with that email exists',
+//       });
+//     }
 
-    // Compare the provided password with the hashed password in the database
-    const passwordMatch = await bcrypt.compare(password, user.password);
+//     // Compare the provided password with the hashed password in the database
+//     const passwordMatch = await bcrypt.compare(password, user.password);
 
-    if (!passwordMatch) {
-      return res.status(401).json({
-        status: 'fail',
-        message: 'Invalid email or password',
-      });
-    }
+//     if (!passwordMatch) {
+//       return res.status(401).json({
+//         status: 'fail',
+//         message: 'Invalid email or password',
+//       });
+//     }
 
-    res.status(200).json({
-      status: 'success',
-      user: {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        role: user.role,
-        otp_enabled: user.otp_enabled,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
-};
+//     res.status(200).json({
+//       status: 'success',
+//       user: {
+//         id: user.id,
+//         name: user.name,
+//         email: user.email,
+//         role: user.role,
+//         otp_enabled: user.otp_enabled,
+//       },
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       status: 'error',
+//       message: error.message,
+//     });
+//   }
+// };
 
 
-const generateRandomBase32 = () => {
-  const buffer = crypto.randomBytes(15);
-  const base32 = encode(buffer).replace(/=/g, "").substring(0, 24);
-  return base32;
-};
+// const generateRandomBase32 = () => {
+//   const buffer = crypto.randomBytes(15);
+//   const base32 = encode(buffer).replace(/=/g, "").substring(0, 24);
+//   return base32;
+// };
 
-const GenerateOTP = async (req: Request, res: Response) => {
-  try {
-    const { user_id } = req.body;
+// const GenerateOTP = async (req: Request, res: Response) => {
+//   try {
+//     const { user_id } = req.body;
 
-    const user = await prisma.user.findUnique({ where: { id: user_id } });
+//     const user = await prisma.user.findUnique({ where: { id: user_id } });
 
-    if (!user) {
-      return res.status(404).json({
-        status: "fail",
-        message: "No user with that email exists",
-      });
-    }
+//     if (!user) {
+//       return res.status(404).json({
+//         status: "fail",
+//         message: "No user with that email exists",
+//       });
+//     }
 
-    const base32_secret = generateRandomBase32();
+//     const base32_secret = generateRandomBase32();
 
-    let totp = new OTPAuth.TOTP({
-      issuer: "meet-clone.com",
-      label: "AICTE meeting",
-      algorithm: "SHA1",
-      digits: 6,
-      secret: base32_secret,
-    });
+//     let totp = new OTPAuth.TOTP({
+//       issuer: "meet-clone.com",
+//       label: "AICTE meeting",
+//       algorithm: "SHA1",
+//       digits: 6,
+//       secret: base32_secret,
+//     });
 
-    let otpauth_url = totp.toString();
+//     let otpauth_url = totp.toString();
 
-    await prisma.user.update({
-      where: { id: user_id },
-      data: {
-        otp_auth_url: otpauth_url,
-        otp_base32: base32_secret,
-      },
-    });
+//     await prisma.user.update({
+//       where: { id: user_id },
+//       data: {
+//         otp_auth_url: otpauth_url,
+//         otp_base32: base32_secret,
+//       },
+//     });
 
-    res.status(200).json({
-      base32: base32_secret,
-      otpauth_url,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "error",
-      message: error.message,
-    });
-  }
-};
+//     res.status(200).json({
+//       base32: base32_secret,
+//       otpauth_url,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       status: "error",
+//       message: error.message,
+//     });
+//   }
+// };
 
-const VerifyOTP = async (req: Request, res: Response) => {
-  try {
-    const { user_id, token } = req.body;
+// const VerifyOTP = async (req: Request, res: Response) => {
+//   try {
+//     const { user_id, token } = req.body;
 
-    const user = await prisma.user.findUnique({ where: { id: user_id } });
-    const message = "Token is invalid or user doesn't exist";
-    if (!user) {
-      return res.status(401).json({
-        status: "fail",
-        message,
-      });
-    }
+//     const user = await prisma.user.findUnique({ where: { id: user_id } });
+//     const message = "Token is invalid or user doesn't exist";
+//     if (!user) {
+//       return res.status(401).json({
+//         status: "fail",
+//         message,
+//       });
+//     }
 
-    let totp = new OTPAuth.TOTP({
-      issuer: "meet-clone.com",
-      label: "AICTE meeting",
-      algorithm: "SHA1",
-      digits: 6,
-      secret: user.otp_base32!,
-    });
+//     let totp = new OTPAuth.TOTP({
+//       issuer: "meet-clone.com",
+//       label: "AICTE meeting",
+//       algorithm: "SHA1",
+//       digits: 6,
+//       secret: user.otp_base32!,
+//     });
 
-    let delta = totp.validate({ token });
+//     let delta = totp.validate({ token });
 
-    if (delta === null) {
-      return res.status(401).json({
-        status: "fail",
-        message,
-      });
-    }
+//     if (delta === null) {
+//       return res.status(401).json({
+//         status: "fail",
+//         message,
+//       });
+//     }
 
-    const updatedUser = await prisma.user.update({
-      where: { id: user_id },
-      data: {
-        otp_enabled: true,
-        otp_verified: true,
-      },
-    });
+//     const updatedUser = await prisma.user.update({
+//       where: { id: user_id },
+//       data: {
+//         otp_enabled: true,
+//         otp_verified: true,
+//       },
+//     });
 
-    res.status(200).json({
-      otp_verified: true,
-      user: {
-        id: updatedUser.id,
-        name: updatedUser.name,
-        email: updatedUser.email,
-        otp_enabled: updatedUser.otp_enabled,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "error",
-      message: error.message,
-    });
-  }
-};
+//     res.status(200).json({
+//       otp_verified: true,
+//       user: {
+//         id: updatedUser.id,
+//         name: updatedUser.name,
+//         email: updatedUser.email,
+//         otp_enabled: updatedUser.otp_enabled,
+//       },
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       status: "error",
+//       message: error.message,
+//     });
+//   }
+// };
 
-const ValidateOTP = async (req: Request, res: Response) => {
-  try {
-    const { user_id, token } = req.body;
-    const user = await prisma.user.findUnique({ where: { id: user_id } });
+// const ValidateOTP = async (req: Request, res: Response) => {
+//   try {
+//     const { user_id, token } = req.body;
+//     const user = await prisma.user.findUnique({ where: { id: user_id } });
 
-    const message = "Token is invalid or user doesn't exist";
-    if (!user) {
-      return res.status(401).json({
-        status: "fail",
-        message,
-      });
-    }
-    let totp = new OTPAuth.TOTP({
-      issuer: "meet-clone.com",
-      label: "AICTE meeting",
-      algorithm: "SHA1",
-      digits: 6,
-      secret: user.otp_base32!,
-    });
+//     const message = "Token is invalid or user doesn't exist";
+//     if (!user) {
+//       return res.status(401).json({
+//         status: "fail",
+//         message,
+//       });
+//     }
+//     let totp = new OTPAuth.TOTP({
+//       issuer: "meet-clone.com",
+//       label: "AICTE meeting",
+//       algorithm: "SHA1",
+//       digits: 6,
+//       secret: user.otp_base32!,
+//     });
 
-    let delta = totp.validate({ token, window: 1 });
+//     let delta = totp.validate({ token, window: 1 });
 
-    if (delta === null) {
-      return res.status(401).json({
-        status: "fail",
-        message,
-      });
-    }
+//     if (delta === null) {
+//       return res.status(401).json({
+//         status: "fail",
+//         message,
+//       });
+//     }
 
-    res.status(200).json({
-      otp_valid: true,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "error",
-      message: error.message,
-    });
-  }
-};
+//     res.status(200).json({
+//       otp_valid: true,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       status: "error",
+//       message: error.message,
+//     });
+//   }
+// };
 
-const DisableOTP = async (req: Request, res: Response) => {
-  try {
-    const { user_id } = req.body;
+// const DisableOTP = async (req: Request, res: Response) => {
+//   try {
+//     const { user_id } = req.body;
 
-    const user = await prisma.user.findUnique({ where: { id: user_id } });
-    if (!user) {
-      return res.status(401).json({
-        status: "fail",
-        message: "User doesn't exist",
-      });
-    }
+//     const user = await prisma.user.findUnique({ where: { id: user_id } });
+//     if (!user) {
+//       return res.status(401).json({
+//         status: "fail",
+//         message: "User doesn't exist",
+//       });
+//     }
 
-    const updatedUser = await prisma.user.update({
-      where: { id: user_id },
-      data: {
-        otp_enabled: false,
-      },
-    });
+//     const updatedUser = await prisma.user.update({
+//       where: { id: user_id },
+//       data: {
+//         otp_enabled: false,
+//       },
+//     });
 
-    res.status(200).json({
-      otp_disabled: true,
-      user: {
-        id: updatedUser.id,
-        name: updatedUser.name,
-        email: updatedUser.email,
-        otp_enabled: updatedUser.otp_enabled,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "error",
-      message: error.message,
-    });
-  }
-};
+//     res.status(200).json({
+//       otp_disabled: true,
+//       user: {
+//         id: updatedUser.id,
+//         name: updatedUser.name,
+//         email: updatedUser.email,
+//         otp_enabled: updatedUser.otp_enabled,
+//       },
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       status: "error",
+//       message: error.message,
+//     });
+//   }
+// };
 
-export default {
-  RegisterUser,
-  LoginUser,
-  GenerateOTP,
-  VerifyOTP,
-  ValidateOTP,
-  DisableOTP,
-};
+// export default {
+//   RegisterUser,
+//   LoginUser,
+//   GenerateOTP,
+//   VerifyOTP,
+//   ValidateOTP,
+//   DisableOTP,
+// };
--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
 import cors from "cors";
 // import authRouter from "./routes/auth.route";
@@ -7,7 +7,7 @@ import requests from "./routes/requests.route"
 import morgan from "morgan";
 import mongoose from "mongoose";
 
-export const prisma = new PrismaClient();
+// export const prisma = new PrismaClient();
 const app = express();
 
 async function main() {
@@ -60,12 +60,12 @@ async function main() {
   });
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
